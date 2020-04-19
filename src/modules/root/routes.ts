f07bbbf659964f464a611e41a3adfa90929c00b4
Yash Kumar Verma <yk.verma2000@gmail.com>
2020-04-19 20:08:33 +0530

    Fix excess variables

--- src/modules/root/routes.ts
+++ src/modules/root/routes.ts
@@ -3,10 +3,8 @@ import express, { Request, Response } from 'express'
 const router = express.Router()
 
 router.get('/', (req: Request, res: Response): void => {
-  console.log('ROUTE HIT ')
   res.render('index', {
     siteKey: process.env.SITE_KEY,
-    test: 'FUCK',
   })
 })
 
