78f6ff8c6d8640a73122bb4fd7ca81e85e79d6ff
Yash Kumar Verma <yk.verma2000@gmail.com>
2020-04-19 21:54:14 +0530

    adding client key

--- src/modules/root/routes.ts
+++ src/modules/root/routes.ts
@@ -4,7 +4,7 @@ const router = express.Router()
 
 router.get('/', (req: Request, res: Response): void => {
   res.render('index', {
-    siteKey: process.env.SITE_KEY,
+    siteKey: '6LcuSOsUAAAAAJbI_N5O7-ara18XvfuRA0Y5Ybvg',
   })
 })
 
